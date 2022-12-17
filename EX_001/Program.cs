a42cdf1ec4d0de365e85ab16b36a74639dc41ae5
anna <annagridchina@mail.ru>
2022-12-17 14:30:40 +0700

    001 перенесено в папку

---
+++ EX_001/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+//Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("start"); //начало
+
+int a = 22;
+int b = 3;
+int c = 9;
+int max = 0;
+if (a < b)
+{
+    max = b;
+}
+if (b < c)
+{
+    max = c;
+}
+else
+{
+    max = a;
+};
+ if (a < c)
+ {
+    max = c;
+ }
+Console.WriteLine(max);
+
+Console.WriteLine("end");
---
+++ EX_001/README.md
@@ -0,0 +1,5 @@
+Первая задача: Найти максимальное из трёх чисел
+
+[код](C:\Users\Пользователь\Desktop\Ucheba\EX_001\Program.cs)
+
+[блок схема](C:\Users\Пользователь\Desktop\Ucheba\EX_001\bloc001.drawio.png)
